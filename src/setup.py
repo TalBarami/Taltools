4e77cfb3a81e36dedfd1dd7123ed2c3acd30dd9e
Tal <baramit@post.bgu.ac.il>
2024-10-31 13:00:31 +0200

    Added setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Taltools',
+    version='1.0',
+    packages=['cv', 'io', 'logging'],
+    package_dir={'': 'src'},
+    url='talbarami.com',
+    license='',
+    author='Tal Barami',
+    author_email='talbaramii@gmail.com',
+    description=''
+)
