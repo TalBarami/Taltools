10bab1e0865a46f35f914159a91a6929047f770a
Tal Barami <baramit@post.bgu.ac.il>
2025-12-23 12:31:01 +0200

    Updated setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Taltools',
+    version='1.0',
+    packages=['taltools', 'taltools.cv', 'taltools.io', 'taltools.data', 'taltools.logging'],
+    url='https://github.com/TalBarami/Taltools',
+    license='MIT',
+    author='Tal Barami',
+    author_email='baramit@post.bgu.ac.il',
+    description='Skeleton and CV utilities'
+)
